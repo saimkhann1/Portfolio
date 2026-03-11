13574fcfbde7f73a2854abdaa85b3bb26eab47d2
Saim Khan <saimkhannoffical@gmail.com>
2026-03-11 18:26:13 +0500

    feat: Last Error Solved

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,7 +1,7 @@
 // src/components/ProjectsSection.jsx
 import React from 'react';
 import ProjectCard from './ProjectCard';
-import { projectsData } from '../data/projects.js';
+import { projectsData } from '../data/projects.jsx';
 
 const ProjectsSection = () => {
   return (
